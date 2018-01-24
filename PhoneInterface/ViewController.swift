5546aa93da9009f6cacde918365c34348e32df06
jinglicase <jingli@case.edu>
2018-01-24 10:42:27 -0500

    Initial Commit

---
+++ PhoneInterface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhoneInterface
+//
+//  Created by Jing Li on 1/24/18.
+//  Copyright © 2018 Jing Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
