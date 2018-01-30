4359cead64e25648bccd96b3b4c03d25b50cdee8
Alex Lucas <alexander.lucas@case.edu>
2018-01-30 16:24:44 -0500

    adding everything

--- PhoneInterface/ViewController.swift
+++ PhoneInterface/ViewController.swift
@@ -10,16 +10,136 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberView: UIView!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myCallButton: UIButton!
+    @IBOutlet weak var endCallButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    var inCall = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        numberView.layer.cornerRadius = numberView.frame.width / 2;
+        endCallButton.isHidden = true;
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func digitPressed(_ sender: UIButton) {
+        if inCall {
+            return
+        }
+        
+        var str = myLabel.text! + sender.currentTitle!
+        
+        switch (str.count) {
+        case 4:
+            if(!str.contains("*") && !str.contains("#")){
+                str.insert("-", at: (str.index((str.startIndex), offsetBy: 3)))
+
+            }
+        case 9:
+            if(!str.contains("*") && !str.contains("#")){
+                str.remove(at: (str.index((str.startIndex), offsetBy: 3)))
+                str.insert("(", at: str.startIndex)
+                str.insert(")", at: str.index(str.startIndex, offsetBy: 4))
+                str.insert(" ", at: str.index(str.startIndex, offsetBy: 5))
+                str.insert("-", at: str.index(str.startIndex, offsetBy: 9))
+            }
+           
+        case 14:
+            if (str.prefix(1) == "(" && !str.contains("*") && !str.contains("#")){
+                myCallButton.isEnabled = true
+
+            }
+
+        case 15:
+            if (str.prefix(1) == "(" && !str.contains("*") && !str.contains("#")){
+                str.remove(at: (str.index((str.startIndex), offsetBy: 9)))
+                str.remove(at: (str.index((str.startIndex), offsetBy: 5)))
+                str.remove(at: (str.index((str.startIndex), offsetBy: 4)))
+                str.remove(at: str.startIndex)
+                myCallButton.isEnabled = false
+
+            }
+
+        default:
+            break
+        }
+        
+        myLabel.text = str
+        deleteButton.isEnabled = true
+       
     }
+    
+    @IBAction func makeCall(_ sender: UIButton) {
+        
+        if !(myLabel.text?.contains("#"))! && !(myLabel.text?.contains("*"))! {
+            endCallButton.isHidden = false
+            myCallButton.isHidden = true
+
+            myLabel.text = "call initiated..."
+            inCall = true
+            deleteButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func endCall(_ sender: UIButton) {
+        endCallButton.isHidden = true
+        myCallButton.isHidden = false
+        
+        myLabel.text = ""
+
+    }
+    
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+       
+
+        var str = myLabel.text!.prefix((myLabel.text?.count)! - 1)
+        
+        switch str.count {
+        case 11:
+            if(str.contains("-") ){
+                str.remove(at: (str.index((str.startIndex), offsetBy: 9)))
+                str.remove(at: (str.index((str.startIndex), offsetBy: 5)))
+                str.remove(at: (str.index((str.startIndex), offsetBy: 4)))
+                str.remove(at: (str.index((str.startIndex), offsetBy: 0)))
+                str.insert("-", at: (str.index((str.startIndex), offsetBy: 3)))
+                
+            }
+           
+        case 10:
+            if(!str.contains("-") && !str.contains("*") && !str.contains("#")){
+                str.insert("(", at: str.startIndex)
+                str.insert(")", at: str.index(str.startIndex, offsetBy: 4))
+                str.insert(" ", at: str.index(str.startIndex, offsetBy: 5))
+                str.insert("-", at: str.index(str.startIndex, offsetBy: 9))
+                myCallButton.isEnabled = true
+
+            }
+
+        case 13:
+            myCallButton.isEnabled = false
+            
+        case 4:
+            if( !str.contains("*") && !str.contains("#")){
+                str.remove(at: (str.index((str.startIndex), offsetBy: 3)))
+
+            }
+
+        case 0:
+            deleteButton.isEnabled = false
 
 
+        default:
+            break
+        }
+        
+        
+        
+        myLabel.text = String(str)
+
+    }
 }
 
